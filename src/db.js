fad292423db88fea64f847cb1aff3da2c5551d04
KalleSoderberg20 <kallewings20@hotmail.se>
2025-05-23 10:32:43 +0200

    Fixed VG hopefully

--- src/db.js
+++ src/db.js
@@ -1,20 +1,40 @@
-/*
-Importerar moduler
-*/
+// /*
+// Importerar moduler
+// */
+// const { Sequelize } = require('sequelize');
+
+// /*
+// Skapar en ny databasanslutning (Sequelize-instans)
+// Använder vi SQLite som databas
+// */
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: './database.sqlite', 
+//   logging: false
+// });
+
+// /*
+// Exporterar 
+// */
+// module.exports = sequelize;
+
+// databas.js
+
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-/*
-Skapar en ny databasanslutning (Sequelize-instans)
-Använder vi SQLite som databas
-*/
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', 
-  logging: false
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,       // databasnamn
+  process.env.DB_USER,       // användarnamn
+  process.env.DB_PASSWORD,   // lösenord
+  {
+    host: process.env.DB_HOST,   // t.ex. 'localhost'
+    port: process.env.DB_PORT,   // t.ex. 5432
+    dialect: 'postgres',
+    logging: false,
+  }
+);
 
-/*
-Exporterar 
-*/
 module.exports = sequelize;
+
 
